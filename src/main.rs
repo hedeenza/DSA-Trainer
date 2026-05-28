4fc6bedfc7f5c42e811c652a5c695fa0f199c7fc
Zach Hedeen <hedeenza@gmail.com>
2026-05-28 09:17:12 -0400

    print the whole solution after the file is done

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,19 @@ use std::fs::File;
 use std::io::{BufReader, BufRead};
 
 fn main() {
-    let file = File::open("test.txt").unwrap();
-    let reader = BufReader::new(file);
+    let algorithm = File::open("test.txt").unwrap();
+    let algorithm_reader = BufReader::new(algorithm);
 
-    for line in reader.lines() {
+    for (index, line) in algorithm_reader.lines().enumerate() {
+        let line = line.unwrap();
+        println!("{}|{}", index + 1, line);
+    }
+
+
+    let solution = File::open("test.txt").unwrap();
+    let solution_reader = BufReader::new(solution);
+    println!("\n[ SOLUTION ]");
+    for line in solution_reader.lines() {
         let line = line.unwrap();
         println!("{}", line);
     }
